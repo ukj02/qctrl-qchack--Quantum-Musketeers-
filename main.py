7c2ba569b0e24ac31827469baa1b415e0ebcc829
tvwest <tvwest@umich.edu>
2021-04-10 15:27:36 -0400

    Fixed basic pulse duration

--- main.py
+++ main.py
@@ -9,22 +9,28 @@ plt.style.use(get_qctrl_style())
 # Start a session with the API.
 qctrl = Qctrl()
 
-control_count = 1
+control_count = 5
 segment_count = 1
-duration = 30.0
-shot_count = 32
+shot_count = 1000
 
 max_rabi_rate = 20 * 2 * np.pi  # MHz
 
-
 controls = []
-# for k in range(control_count):
-#     # Create a random string of complex numbers for each controls.
-#     real_part = np.random.random(size=[segment_count])
-#     imag_part = np.random.random(size=[segment_count])
-#     values = 0.15 * k * (real_part + 1j * imag_part)
+for k in range(control_count):
+    controls.append({"duration": (np.pi / max_rabi_rate)*1000, "values": np.array([1])})
 
-controls.append({"duration": np.pi / max_rabi_rate, "values": np.array([1])})
+with qctrl.create_graph() as graph:
+    with graph:
+        variables = qctrl.operations.bounded_optimization_variable(
+            count=10,
+            lower_bound=0,
+            upper_bound=1,
+        )
+
+
+
+
+
 
 
 experiment_results = qctrl.functions.calculate_qchack_measurements(
@@ -42,4 +48,12 @@ for k, measurement_counts in enumerate(measurements):
     p1 = measurement_counts.count(1) / shot_count
     p2 = measurement_counts.count(2) / shot_count
     print(f"control #{k}: P(|0>) = {p0:.2f}, P(|1>) = {p1:.2f}, P(|2>) = {p2:.2f}")
+
+cost = 0
+for i in range(control_count):
+    cost = cost + (p0-0)**2 + (p1-1)**2 + (p2-0)**2
+print(cost)
+
+
+
 
